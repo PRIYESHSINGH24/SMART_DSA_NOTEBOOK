b2cdab491750cec672efb61bf551b906e5355493
Arisha Firoz <169101584+Arisha2902@users.noreply.github.com>
2025-01-19 00:08:40 +0530

    ok

--- LeetCode/230. Kth Smallest Element in a BST.c
+++ LeetCode/230. Kth Smallest Element in a BST.c
@@ -1,11 +1,10 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
+ * Definition for a binary tree node.*/
+ struct TreeNode {
+      int val;
+      struct TreeNode *left;
+      struct TreeNode *right;
+  };
 
 #include <stdbool.h>
 
