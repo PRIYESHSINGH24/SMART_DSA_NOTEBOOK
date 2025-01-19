99ae34cab67ae9aa156cc6def07b542431f55ecb
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-01-19 12:46:13 +0530

    Create Third largest element.py

---
+++ Geeks For Geeks/Third largest element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def thirdLargest(self,arr):
+        # code here
+        arr.sort()
+        if len(arr) == 2:
+            return arr[-1]
+        elif len(arr) == 1:
+            return arr[0]
+        elif len(arr)>=3:
+            return arr[-3]
