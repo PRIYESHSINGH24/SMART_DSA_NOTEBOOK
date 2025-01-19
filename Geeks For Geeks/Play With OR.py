807870eda716c0eb03700d2535406985cbd27de8
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-01-19 12:48:25 +0530

    Update Play With OR.py

--- Geeks For Geeks/Play With OR.py
+++ Geeks For Geeks/Play With OR.py
@@ -1,6 +1,5 @@
 class Solution:
-    def game_with_number (self, arr,  n) : 
-        #Complete the function
+    def game_with_number (self, arr,  n): 
         l = []
         for i in range(n-1):
             l.append(arr[i] | arr[i+1])
