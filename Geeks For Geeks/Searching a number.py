9a01349a64d2c562e9e39af82b0a9f856346479f
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-01-19 12:50:32 +0530

    Update Searching a number.py

--- Geeks For Geeks/Searching a number.py
+++ Geeks For Geeks/Searching a number.py
@@ -1,8 +1,6 @@
 from typing import List
-
 class Solution:
     def search(self, k: int, arr: List[int]) -> int:
-        # code here
         if k in arr:
             return arr.index(k)+1
         else:
