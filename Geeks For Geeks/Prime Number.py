5012501b89d98c81f739d18139341a9a93985b3e
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-01-19 12:48:40 +0530

    Update Prime Number.py

--- Geeks For Geeks/Prime Number.py
+++ Geeks For Geeks/Prime Number.py
@@ -1,7 +1,5 @@
-
 class Solution:
     def isPrime(self, n):
-        # Function to check if a number 'n' is prime
         for i in range(2, n):
             if n % i == 0:
                 return False
