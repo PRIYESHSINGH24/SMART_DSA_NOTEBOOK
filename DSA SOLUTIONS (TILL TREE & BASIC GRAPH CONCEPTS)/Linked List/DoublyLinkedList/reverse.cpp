49012569072ff1282bbfb850e8a1809d07c8f4c9
yashpandav <pandavyash076@gmail.com>
2025-01-20 17:18:55 +0530

    Reverse Doubly LinkedList

---
+++ DSA SOLUTIONS (TILL TREE & BASIC GRAPH CONCEPTS)/Linked List/DoublyLinkedList/reverse.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+Node* reverseList(Node* head) {
+    Node *prev = NULL, *current = head, *next = NULL;
+
+    while (current != NULL) {
+        next = current->next; 
+        current->next = prev; 
+        prev = current;       
+        current = next;       
+    }
+
+    return prev; 
+}
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void insertAtEnd(Node*& head, int data) {
+    Node* newNode = new Node(data);
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+int main() {
+    Node* head = NULL;
+
+    insertAtEnd(head, 1);
+    insertAtEnd(head, 2);
+    insertAtEnd(head, 3);
+    insertAtEnd(head, 4);
+    insertAtEnd(head, 5);
+
+    cout << "Original Linked List:\n";
+    printList(head);
+
+    head = reverseList(head);
+
+    cout << "Reversed Linked List:\n";
+    printList(head);
+
+    return 0;
+}
