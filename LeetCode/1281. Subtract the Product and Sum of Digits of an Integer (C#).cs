712d86a8a1c6acfb21eb5a56718951ca1aa33693
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-01-20 08:50:21 +0530

    Create 1281. Subtract the Product and Sum of Digits of an Integer (C#).cs

---
+++ LeetCode/1281. Subtract the Product and Sum of Digits of an Integer (C#).cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int SubtractProductAndSum(int n) {
+        int rem = 0;        
+        int sum_n = 0;     
+        int product_n = 1;  
+
+        while(n > 0) {
+            rem = n % 10;  
+            sum_n += rem;  
+            product_n *= rem; 
+            n /= 10;        
+        }
+        
+        return product_n - sum_n;
+        
+    }
+}
