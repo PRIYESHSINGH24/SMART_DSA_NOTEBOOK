24bd980346697453d8524954307d826aae727793
Priyesh Singh <priyeshsingh571@gmail.com>
2025-01-22 00:14:41 +0530

    Your commit message here

--- Geeks For Geeks/Array Subset.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isSubset(self, a, b):
-        set_a = set(a)
-
-        for element in b:
-            if element not in set_a:
-                return False
-        return True
