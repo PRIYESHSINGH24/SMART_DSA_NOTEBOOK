f875866572c0de2234b16cf0b6a1aa5cbe334d1e
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-01-21 21:38:59 +0530

    Update Print first letter of every word in the string.py

--- Geeks For Geeks/Print first letter of every word in the string.py
+++ Geeks For Geeks/Print first letter of every word in the string.py
@@ -1,8 +1,7 @@
 class Solution:
-	def firstAlphabet(self, S):
-		# code here
-		first_word = ""
-		S = S.split()
-		for i in S:
-		    first_word = first_word + i[0]
-		return first_word
+    def firstAlphabet(self, S):
+	first_word = ""
+	S = S.split()
+	for i in S:
+	    first_word = first_word + i[0]
+	return first_word
