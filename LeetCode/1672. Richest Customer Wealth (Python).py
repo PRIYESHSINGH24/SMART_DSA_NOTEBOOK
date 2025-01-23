0b0c7ede438ebb54c0ae48ff6c5dd4fd0dbaa65b
PRIYESH SINGH <priyeshsingh571@gmail.com>
2025-01-23 16:10:40 +0530

    removed

--- LeetCode/1672. Richest Customer Wealth (Python).py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def maximumWealth(self, accounts):
-        wealth = sum(accounts[0]) 
-
-        for i in accounts:
-            if sum(i) > wealth:
-                wealth = sum(i)
-              
-        return wealth
